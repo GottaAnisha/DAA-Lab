b9a0d906c56e453fe687ee6432b310a7d339cc65
GottaAnisha <gottaanishapatro@gmail.com>
2026-08-11 21:03:11 +0530

    Add files via upload

---
+++ BsRecursive.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int low, int high, int key)
+{
+    if(low > high)
+    {
+        return -1;
+    }
+
+    int mid = (low + high) / 2;
+
+    if(arr[mid] == key)
+    {
+        return mid;
+    }
+    else if(arr[mid] < key)
+    {
+        return binarySearch(arr, mid + 1, high, key);
+    }
+    else
+    {
+        return binarySearch(arr, low, mid - 1, key);
+    }
+}
+
+int main()
+{
+    int n, key;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements in sorted order: ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    cout << "Enter the element to search: ";
+    cin >> key;
+
+    int result = binarySearch(arr, 0, n - 1, key);
+
+    if(result != -1)
+    {
+        cout << "Element found at position " << result + 1 << endl;
+    }
+    else
+    {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
+}
